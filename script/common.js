59b4b5a1cca8ab3d82cc64cb71fe7195f5c78bc4
jslover <fzuhua@gmail.com>
2012-11-09 14:06:12 +0800

    myskin

---
+++ script/common.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    $('.demo').append('<a href="#" class="btn-show-code">查看源码</a>');
+    $('.btn-show-code').click(function () {
+        var $demo = $(this).parent().clone();
+        $demo.find('a.btn-show-code').remove();
+        if (!$.$codeViewWin) {
+            $.$codeViewWin = $('<div id="code-view-win" style="display:none;"><textarea></textarea><a href="#" class="btn-close-code">x</a></div>').appendTo($('body'));
+            $.$codeViewWin.find('.btn-close-code').click(function () {
+                $.$codeViewWin.fadeOut(200);
+                return false;
+            });
+        }
+        $.$codeViewWin.fadeIn(200).find('textarea').val($demo.html());
+        return false;
+    });
+});
