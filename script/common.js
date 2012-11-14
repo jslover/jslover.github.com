e9fbdbb62af6029ec37b648c22795109a1b0b656
jslover <fzuhua@gmail.com>
2012-11-14 15:42:53 +0800

    优化查看源码功能

--- script/common.js
+++ script/common.js
@@ -1,8 +1,13 @@
-﻿$(function () {
-    $('.demo').append('<a href="#" class="btn-show-code">查看源码</a>');
-    $('.btn-show-code').click(function () {
-        var $demo = $(this).parent().clone();
-        $demo.find('a.btn-show-code').remove();
+﻿$(function () {  
+    $('.demo').each(function (i, item) {
+        var $demo = $('.demo').eq(i);
+        var h = $demo.html();
+        var $btn = $('<a href="#" class="btn-show-code">查看源码</a>').appendTo($demo); 
+        $btn.click(function () {
+            pop(h);
+        });
+    });
+    function pop(msg) {
         if (!$.$codeViewWin) {
             $.$codeViewWin = $('<div id="code-view-win" style="display:none;"><textarea></textarea><a href="#" class="btn-close-code">x</a></div>').appendTo($('body'));
             $.$codeViewWin.find('.btn-close-code').click(function () {
@@ -10,7 +15,6 @@
                 return false;
             });
         }
-        $.$codeViewWin.fadeIn(200).find('textarea').val($demo.html());
-        return false;
-    });
+        $.$codeViewWin.fadeIn(200).find('textarea').val(msg);
+    }
 });
